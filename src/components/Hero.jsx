065526aa1e13f715c2fb321d00d571d10b37c618
02mehdii <107004006+02mehdii@users.noreply.github.com>
2023-09-06 15:59:02 -0400

    Link Added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/02mehdii/ai-summerizer", "_blank")
           }
           className='black_btn'
         >
